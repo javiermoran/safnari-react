de258af2b5eb7a8cfb432d98b002ca897553608c
Javier Moran <jmoranm1987@gmail.com>
2019-06-02 21:42:16 -0500

    Redux Added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React from 'react';
 import { Router, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import * as history from 'history';
 import './App.scss';
 import HomePage from './components/home/HomePage';
 import Header from './components/header/Header';
 import Login from './components/login/Login';
 import Register from './components/register/Register';
+import configureStore from './store/configureStore';
 
 const browserHistory = history.createBrowserHistory();
+const store = configureStore();
 
 const App = () => (
-  <Router history={browserHistory}>
-    <div className="App">
-      <Header />
-      <Switch>
-        <Route path="/" component={HomePage} exact={true} />
-        <Route path="/login" component={Login} />
-        <Route path="/register" component={Register} />
-      </Switch>
-    </div>
-  </Router>
+  <Provider store={store}>
+    <Router history={browserHistory}>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route path="/" component={HomePage} exact={true} />
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+        </Switch>
+      </div>
+    </Router>
+  </Provider>
 );
 
 export default App;
---
+++ src/store/configureStore.js
@@ -0,0 +1,11 @@
+import { createStore, combineReducers } from 'redux';
+
+export default () => {
+  const store = createStore(
+    combineReducers({
+
+    })
+  );
+
+  return store;
+}
